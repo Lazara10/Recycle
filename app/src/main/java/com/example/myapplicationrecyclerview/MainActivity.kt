4e9ccfd63c2beb8e7ca59867c94e42693e967dc3
lukaguruli <luka.guruli.1@btu.edu.ge>
2021-01-27 12:38:24 +0400

    first commit

--- app/src/main/java/com/example/myapplicationrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationrecyclerview/MainActivity.kt
@@ -11,12 +11,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val images = listOf<image>(
-            image("Images1", R.drawable.boot),
+            image("Images1", R.drawable.cprogrr),
             image("Images2", R.drawable.cplu),
-            image("Images3", R.drawable.php),
-            image("Images4", R.drawable.ping),
-            image("Images5", R.drawable.cprogrr),
-            image("Images6", R.drawable.java),
+            image("Images3", R.drawable.java),
+            image("Images4", R.drawable.phyton),
+            image("Images5", R.drawable.boot),
+            image("Images6", R.drawable.aspn),
+            image("Images6", R.drawable.php),
+            image("Images6", R.drawable.ping),
         )
         val recyclerView = findViewById<RecyclerView>(R.id.ImagesRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
